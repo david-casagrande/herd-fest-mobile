897c905630fb20640116c8dc77fca85495425967
David Casagrande <dncasagrande@gmail.com>
2016-09-12 12:56:31 -0400

    added ability to save device ids and tokens on app launch

--- ios/HerdFest/AppDelegate.m
+++ ios/HerdFest/AppDelegate.m
@@ -31,7 +31,68 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [self registerForRemoteNotifications];
+
   return YES;
 }
+
+// Handle remote notification registration.
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
+  [self saveDevToken:devToken];
+}
+
+- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
+  NSLog(@"Error in registration. Error: %@", err);
+}
+
+- (void) registerForRemoteNotifications {
+  // Register the supported interaction types.
+  UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+  UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+  [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+  
+  // Register for remote notifications.
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+
+- (void) saveDevToken:(NSData *)devToken {
+  NSString *uuid = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+  NSString *apiURL = [[[NSProcessInfo processInfo] environment] objectForKey:@"API_URL"];
+  NSURL *url = [NSURL URLWithString: apiURL ? apiURL : @"https://herd-fest-api.herokuapp.com/api/devices"];
+  
+  NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+  config.HTTPAdditionalHeaders = @{ @"Content-Type"  : @"application/json" };
+  NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+  
+  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+  request.HTTPMethod = @"POST";
+  
+  NSDictionary *dictionary = @{ @"device_id": uuid, @"device_token": [self stringWithDeviceToken:devToken] };
+  
+  NSError *error = nil;
+  NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary options:kNilOptions error:&error];
+  
+  if (!error) {
+    NSURLSessionUploadTask *uploadTask = [session uploadTaskWithRequest:request
+                                                               fromData:data completionHandler:^(NSData *data,NSURLResponse *response,NSError *error) {
+                                                                 NSLog(@"%@", error);
+                                                               }];
+    
+    [uploadTask resume];
+  }
+}
+
+- (NSString *) stringWithDeviceToken:(NSData *)deviceToken {
+  const char *data = [deviceToken bytes];
+  NSMutableString *token = [NSMutableString string];
+  
+  for (NSUInteger i = 0; i < [deviceToken length]; i++) {
+    [token appendFormat:@"%02.2hhX", data[i]];
+  }
+  
+  return [token copy];
+}
+
 
 @end
