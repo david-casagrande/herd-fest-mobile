30e33f7ac32ae52652b1c955b7eb65d1c800543d
David Casagrande <dncasagrande@gmail.com>
2017-06-08 23:00:32 -0400

    updated 2017 colors

--- app/styles/components/colors.js
+++ app/styles/components/colors.js
@@ -1,11 +1,11 @@
 const secondary = '#444';
 const lightGrey = '#b0b1b2';
 
-const green = '#7ec583';
-const purple = '#3e2a86';
-const red = '#ad181a';
-const teal = '#81cbc3';
-const blue = '#1680c7';
+const green = '#9bd5c5'; // #7ec583;
+const purple = '#f69682'; // #3e2a86;
+const red = '#f4d3c8'; // #ad181a;
+const teal = '#a4dbde'; // #81cbc3;
+const blue = '#174799'; // #1680c7;
 
 const white = '#ffffff';
 
@@ -15,5 +15,5 @@ export default {
   secondary,
   lightGrey,
   white,
-  pinWheel: [green, purple, blue, teal, red]
+  pinWheel: [green, purple, teal, red, blue]
 };
