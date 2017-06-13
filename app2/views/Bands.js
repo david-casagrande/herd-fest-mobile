06345d1b21b1701496e1e373eabd16420fe53414
David Casagrande <dncasagrande@gmail.com>
2017-06-13 18:51:46 -0400

    update views with container

--- app2/views/Bands.js
+++ app2/views/Bands.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HFFlatList from '../components/HFFlatList';
+import HFContainer from '../components/HFContainer';
 
 class BandsView extends React.Component {
   render() {
@@ -11,7 +12,11 @@ class BandsView extends React.Component {
       onPress: (item) => this.props.navigation.navigate('Band', item)
     };
 
-    return <HFFlatList {...props} />;
+    return (
+      <HFContainer>
+        <HFFlatList {...props} />
+      </HFContainer>
+    );
   }
 }
 
--- app2/views/Venues.js
+++ app2/views/Venues.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HFFlatList from '../components/HFFlatList';
+import HFContainer from '../components/HFContainer';
 
 class VenuesView extends React.Component {
   render() {
@@ -11,7 +12,11 @@ class VenuesView extends React.Component {
       onPress: (item) => this.props.navigation.navigate('Venue', item)
     };
 
-    return <HFFlatList {...props} />;
+    return (
+      <HFContainer>
+        <HFFlatList {...props} />
+      </HFContainer>
+    );
   }
 }
 
