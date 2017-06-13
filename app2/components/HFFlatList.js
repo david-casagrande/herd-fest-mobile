6bf316d510d3993b540d8b6ab7ea4628bee5e482
David Casagrande <dncasagrande@gmail.com>
2017-06-13 17:38:13 -0400

    added HFFlatList test

---
+++ app2/components/HFFlatList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FlatList, Text, View, TouchableOpacity } from 'react-native';
+import styles from '../styles/hf-flat-list';
+
+class HFFlatList extends React.Component {
+  item({ item }) {
+    return (
+      <TouchableOpacity onPress={() => this.props.onPress(item)} style={styles.item}>
+        <Text style={styles.label}>{item[this.props.labelProp]}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    const props = {
+      data: this.props.data,
+      keyExtractor: (item) => item[this.props.keyProp],
+      renderItem: (info) => this.item(info),
+      ItemSeparatorComponent: () => <View style={styles.separator} />
+    };
+
+    return <FlatList {...props} />;
+  }
+}
+
+HFFlatList.propTypes = {
+  data: PropTypes.array.isRequired,
+  keyProp: PropTypes.string.isRequired,
+  labelProp: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired
+};
+
+export default HFFlatList;
---
+++ app2/components/__tests__/HFFlatList-test.js
@@ -0,0 +1,60 @@
+import 'react-native';
+import React from 'react';
+import HFFlatList from '../HFFlatList';
+import { shallow } from 'enzyme';
+
+jest.mock('react-native', () => ({
+  FlatList: () => false,
+  StyleSheet: {},
+  TouchableOpacity: () => false,
+  Text: () => false,
+  View: () => false
+}));
+
+describe('HFFlatList', () => {
+  let props = null;
+
+  beforeEach(() => {
+    props = {
+      data: [{ id: '1', name: 'Test' }],
+      keyProp: 'id',
+      labelProp: 'name',
+      onPress: jest.fn()
+    };
+  });
+
+  it('renders a FlatList', () => {
+    const wrapper = shallow(<HFFlatList {...props} />);
+    const list = wrapper.find('FlatList');
+
+    expect(list.prop('data')).toEqual(props.data);
+  });
+
+  it('keyExtractor based on props.keyProp', () => {
+    const wrapper = shallow(<HFFlatList {...props} />);
+    const list = wrapper.find('FlatList');
+
+    expect(list.props().keyExtractor(props.data[0])).toEqual('1');
+  });
+
+  it('item', () => {
+    const wrapper = shallow(<HFFlatList {...props} />);
+    const list = wrapper.find('FlatList');
+    const item = list.props().renderItem({ item: props.data[0] });
+    const label = item.props.children;
+
+    expect(label.props.children).toEqual('Test');
+
+    item.props.onPress();
+
+    expect(props.onPress).toBeCalledWith(props.data[0]);
+  });
+
+  it('separator', () => {
+    const wrapper = shallow(<HFFlatList {...props} />);
+    const list = wrapper.find('FlatList');
+    const separator = list.props().ItemSeparatorComponent();
+
+    expect(separator.props.style).toBeDefined();
+  });
+});
