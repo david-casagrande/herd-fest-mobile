2ee15d63f4720da59a2ef40b68110645d2d5cf0d
David Casagrande <dcasagrande@secureauth.com>
2017-06-21 17:14:21 -0400

    updted veune view

--- app2/views/Venue.js
+++ app2/views/Venue.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text } from 'react-native';
 import HFContainer from '../components/HFContainer';
+import HFSectionList from '../components/HFSectionList';
+import HFSetTime from '../components/HFSetTime';
 
 class VenueView extends React.Component {
   render() {
     return (
       <HFContainer>
-        <Text>WUT UP</Text>
+        <HFSectionList
+          keyProp="id"
+          sections={this.props.sections}
+          renderItem={({ item }) => <HFSetTime setTime={item} onPress={() => this.props.onNavigate('Band', item.band)} />}
+          renderSectionHeader={({ section }) => section.name}
+        />
       </HFContainer>
     );
   }
@@ -15,8 +21,9 @@ class VenueView extends React.Component {
 
 VenueView.propTypes = {
   venue: PropTypes.shape({
-    set_times: PropTypes.array.isRequired
-  }).isRequired
+  }).isRequired,
+  sections: PropTypes.array.isRequired,
+  onNavigate: PropTypes.func.isRequired
 };
 
 export default VenueView;
